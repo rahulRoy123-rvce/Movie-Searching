585f6ff0940bf2c04c4e0daa68e1971ff7c5296f
roydecoder69 <97295666+roydecoder69@users.noreply.github.com>
2022-07-30 23:57:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+import './App.css';
+import searchIcon from './search.svg';
+import MovieCard from './MovieCard';
+//7a119829
+
+const API_URL = 'http://www.omdbapi.com?apikey=7a119829';
+
+const movie1 = {
+    
+        "Title": "Amazing Spiderman Syndrome",
+        "Year": "2012",
+        "imdbID": "tt2586634",
+        "Type": "movie",
+        "Poster": "N/A"
+    }
+
+
+const App = ()=>{
+
+    const [movies, setMovies]= useState([]);
+    const [searchTerm, setSearchterm] = useState('');
+
+    const searchMovies = async(title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data =await response.json();
+
+        setMovies(data.Search);
+    }
+ 
+   useEffect(()=>{
+    searchMovies('Spiderman');
+
+   }, []);
+    return (
+        <div className='app'>
+            <h1>MovieLand</h1>
+
+            <div className='search'>
+                <input
+                  placeholder='Search for movies'
+                  value={searchTerm}
+                  onChange={(e)=> setSearchterm(e.target.value)}
+                  
+                  />
+                  <img
+                   src={searchIcon}
+                   alt="Search"
+                   onClick={()=> searchMovies(searchTerm)}
+                   />
+            </div>
+
+            {
+                movies?.length>0 
+                ?
+                (<div className="container">
+                  {movies.map((movie)=>(
+                    <MovieCard movie={movie}/>
+                  ))}
+                </div>
+                 ):(
+                    <div className="empty">
+                        <h2>No movies found.</h2>
+                    </div>
+                 )
+          
+            }
+
+           
+        </div>
+    );
+}
+
+export default App;
